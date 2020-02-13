9f72da9cbe7edfcac1f6f0002a6fa7df064914c4
emreozb <59108987+emreozb@users.noreply.github.com>
2020-02-13 15:58:48 -0500

    FBI API
    Used FBI API to retrieve data, and created GUI for that JSON file by  importing Requests, Tkinter and JSON libraries in Python.

---
+++ Class_FBI.py
@@ -0,0 +1,81 @@
+import json
+import requests
+from tkinter import *
+
+class FBI():
+    def __init__(self, ori, data_year, offense, state, cleared, actual):
+        self.ori = ori
+        self.data_year = data_year
+        self.offense = offense
+        self.state = state
+        self.cleared = cleared
+        self. actual = actual
+
+    def ToList(self):
+        return [self.ori, self.data_year,self.offense, self.state,self.cleared,self.actual]
+
+    def __eq__(self, other):        
+        try:
+            if(isinstance(other, FBI) and self.ToList() == other.ToList()): 
+                return True
+        except:
+            raise ValueError("Type comparison with non-FBI object")
+        
+        return False
+
+    def __repr__(self):
+        raise NotImplementedError
+
+    def __str__(self):
+        return "ori: {},  data_year: {}, offense: {}, state: {}, cleared: {}, actual {}".format(str(self.ori), int(self.data_year), str(self.offense), str(self.state), int(self. cleared), int(self.actual))
+
+    def Contains(self, value):
+        for item in self.ToList():
+            if str(item) == str(value):
+                return True
+        return False
+
+    def DisplayDetailUI(self):
+        root = Tk()
+        root.title('Crime Detail Window')
+        root.geometry('{}x{}'.format(400, 400))
+
+        # layout all of the main containers
+        root.grid_rowconfigure(1, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+
+        # create all of the main containers
+        center = Frame(root, bg='light blue', padx=3, pady=3)
+        center.grid(row=0, sticky="nsew")
+
+        strAPI = "https://api.usa.gov/crime/fbi/sapi/api/summarized/arson/states/" + self.state + "/1970/2018?API_KEY=DGYvtvfuOKwyQnd14wdANvH5QFeltbe2GR6gU1qI"
+        response = requests.get(strAPI)
+        json_data = response.json()
+        crimes_json = json_data['results']
+
+        
+        
+        data_year = json_data.get("results")[0]['data_year']
+        state_id =json_data.get("results")[0]['state_id']
+        state_abbr =json_data.get("results")[0]['state_abbr']
+        reported = json_data.get("results")[0]['reported']
+        unfounded = json_data.get("results")[0]['unfounded']
+        actual = json_data.get("results")[0]['actual']
+        cleared = json_data.get("results")[0]['cleared']
+        juvenile = json_data.get("results")[0]['juvenile_cleared']
+        uninhabited = json_data.get("results")[0]['uninhabited']
+        damage = json_data.get("results")[0]['est_damage_value']
+            
+        Label(center, text= "Year: " + str(data_year), width=20, pady=1, padx=1).grid(row=1, column = 0)
+        Label(center, text= "State ID: " + str(state_id), width=20, pady=1, padx=1).grid(row=2, column = 0)
+        Label(center, text= "State Abbreviation: " + str(state_abbr), width=20, pady=1, padx=1).grid(row=3, column = 0)
+        Label(center, text= "Reported: " + str(reported), width=20, pady=1, padx=1).grid(row=4, column = 0)
+        Label(center, text= "Unfounded: " + str(unfounded), width=20, pady=1, padx=1).grid(row=5, column = 0)
+        Label(center, text= "Actual: " + str(actual), width=20, pady=1, padx=1).grid(row=6, column = 0)
+        Label(center, text= "Cleared: " + str(cleared), width=20, pady=1, padx=1).grid(row=7, column = 0)
+        Label(center, text= "Juvenile:" + str(juvenile), width=20, pady=1, padx=1).grid(row=8, column = 0)
+        Label(center, text= "Uninhabited: " + str(uninhabited), width=20, pady=1, padx=1).grid(row=9, column = 0)
+        Label(center, text= "Damage: " + str(damage), width=20, pady=1, padx=1).grid(row=10, column = 0)
+    
+
+        root.mainloop()
---
+++ fbi_API.py
@@ -0,0 +1,101 @@
+import json
+import requests
+from tkinter import *
+from Class_FBI import FBI
+
+
+real_res = None
+displayElments = []
+root = Tk()
+root.title('Crime Summary')
+root.geometry('{}x{}'.format(900, 600))
+top_frame = Frame(root, bg='black',padx=3, pady=3)
+name = StringVar()
+entry_keyword = Entry(top_frame, textvariable = name, background="green")
+
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+# create all of the main containers
+center = Frame(root, bg='cyan', padx=3, pady=3)
+top_frame.grid(row=0, sticky="ew")
+center.grid(row=1, sticky="nsew")
+
+# create the widgets for the top frame
+model_label = Label(top_frame, text='Filter Crime')
+
+# layout the widgets in the top frame
+model_label.grid(row=0, columnspan=2)
+entry_keyword.grid(row=1, column=0)
+
+states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+
+stateAbbr = ""
+def GetState(stateAbbr):
+    stateAbbr = input("What state? ").upper()
+    while stateAbbr not in states:
+        print("invalid state, please try again")
+        stateAbbr = input("What state? ").upper()
+    return stateAbbr
+stateAbbr = GetState(stateAbbr)
+            
+
+API = "https://api.usa.gov/crime/fbi/sapi/api/summarized/state/" + stateAbbr + "/arson/2000/2018?API_KEY=DGYvtvfuOKwyQnd14wdANvH5QFeltbe2GR6gU1qI"
+response = requests.get(API)
+json_data = response.json()
+crimes_json = json_data['results']
+results = []
+
+for crime in crimes_json:
+    results.append(FBI(crime['ori'], crime['data_year'], crime['offense'], crime['state_abbr'], crime['cleared'],crime['actual']))
+
+
+def reset():
+    arr = center.grid_slaves()
+    for elem in arr:
+        elem.destroy()
+    DisplaySummaryUI(results)
+    root.update()
+
+def FilterCrimes():
+    filtered_text = name.get()
+    real_res = [res for res in results if res.Contains(filtered_text)]
+    
+    arr  = center.grid_slaves()
+    for elment in arr:
+        elment.destroy()
+    DisplaySummaryUI(real_res)
+    root.update()
+   
+    print("The create a filtered crime UI here by keyword:", [str(res) for res in real_res]) 
+
+button = Button(top_frame, text= "Submit", command= FilterCrimes) 
+button.grid(row=1, column=1)
+button2 = Button(top_frame, text="Reset", command=reset)
+button2.grid(row=1, column=2)
+
+i = 0
+for result in results:
+    Button(center, text="Crime Details!", command=result.DisplayDetailUI).grid(row=i, column = 0)
+    Label(center, text=result, width=100, pady=3, padx=3).grid(row=i, column = 1)
+    i+=1
+def DisplaySummaryUI(crimes):
+    i = 0
+    for crime in crimes:
+        Button(center, text="Crime Details!", command=crime.DisplayDetailUI).grid(row=i, column = 0)
+        Label(center, text=crime.ori, width=10, pady=1, padx=1).grid(row=i, column = 1)
+        Label(center, text=crime.data_year, width=10, pady=1, padx=1).grid(row=i, column = 2)
+        Label(center, text=crime.offense, width=20, pady=1, padx=1).grid(row=i, column = 3)
+        Label(center, text=crime.state, width=20, pady=1, padx=1).grid(row=i, column = 4)
+        Label(center, text=crime.cleared, width=20, pady=1, padx=1).grid(row=i, column = 5)
+        Label(center, text=crime.actual, width=20, pady=1, padx=1).grid(row=i, column = 5)
+        i += 1
+root.mainloop()
+
+
+
